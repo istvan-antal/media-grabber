f87992bd54888ff60503894ac42c35af29af6eb9
István Antal <istvan.m.antal@gmail.com>
2018-02-10 14:34:09 +0200

    Initial Electron / TypeScript setup

---
+++ main.ts
@@ -0,0 +1,26 @@
+import { BrowserWindow, app } from 'electron';
+import { join } from 'path';
+
+if (process.mas) {
+    app.setName('Media Grabber');
+}
+
+const createWindow = () => {
+    const mainWindow = new BrowserWindow({
+        width: 1080,
+        minWidth: 680,
+        height: 840,
+        title: app.getName()
+    });
+
+    mainWindow.loadURL(join('file://', __dirname, '/app/index.html'));
+
+    app.on('window-all-closed', function () {
+        app.quit();
+    });
+}
+
+app.on('ready', function () {
+    createWindow()
+})
+
